2428d4c5531cbfd516b90fe74f36c711d0069702
Будилов Максим <lordgodrik@gmail.com>
2022-06-30 00:38:54 +0300

    First steps of parsing website for stories

--- Pikabu_light/Pikabu_light/settings.py
+++ Pikabu_light/Pikabu_light/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import db_conn
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vue*1le6u6dctt$dzp#y-g)@6!t-=pru(#dm&$pii&#2)z5ip3'
+SECRET_KEY = 'django-insecure-$jw9lr1j@i(+h#-_^^eit9f-21sn*^^6k@9d@6bh%0q%-=5k9g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,10 +76,21 @@ WSGI_APPLICATION = 'Pikabu_light.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Pikabu_light',
+        'USER': db_conn.user,
+        'PASSWORD': db_conn.my_password_to_db,
+        'HOST': 'localhost',
+        'PORT': '5050',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print('First push')
+import requests
+import time
+from fake_user_agent.main import user_agent
+from bs4 import BeautifulSoup as BS
+
+ua = user_agent()
+HEADERS = {'user-agent': ua,
+           'accept': '*/*'}
+
+URL = 'https://pikabu.ru/best'
+start_time = time.time()
+
+
+def get_my_data(html=None):
+    all_stories = []
+    soup = BS(html, 'html.parser')
+    stories = soup.find(class_="stories-feed__container").find_all(class_="story__main")
+    print(len(stories))
+    one_story_data = {
+        "news_id": None,
+        "href": None,
+        "author": None,
+        "story_title": None,
+        "story_block": None,
+    }
+
+    for story in stories:
+        if story.find_all(class_="story-block_type_text") \
+                and not story.find_all(class_="story-block_type_image") \
+                and not story.find_all(class_="story-block_type_video"):
+            one_story_data["news_id"] = story.get("data-story-id")
+            one_story_data['href'] = story.find(class_="story__title").find("a").get("href")
+            one_story_data["author"] = story.get("data-profile")
+            one_story_data['story_title'] = story.find(class_="story__title").text
+            one_story_data['story_block'] = story.find(class_="story-block_type_text").text
+            all_stories.append(one_story_data)
+
+    return all_stories
+
+
+if __name__ == '__main__':
+    r = requests.get(URL, headers=HEADERS)
+    rez = get_my_data(r.text)
+    print(rez)
