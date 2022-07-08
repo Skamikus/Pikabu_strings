d96c113bc94c9855e9fd10ea1f9c332ebb60e31c
Maxim Budilov <lordgodrik@gmail.com>
2022-07-08 13:59:17 +0300

    ListView for OOP

--- Pikabu_light/Short_tales/urls.py
+++ Pikabu_light/Short_tales/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    # path('', index, name='home'),
+    path('', HomePosts.as_view(), name='home'),
     path('category/<int:category_id>/', get_category, name='category'),
     path('posts/add-post/', add_post, name='add_post'),
     path('test/', test),
--- Pikabu_light/Short_tales/views.py
+++ Pikabu_light/Short_tales/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.views.generic import ListView
+
 from .models import Posts, Category
 from .forms import PostsForm
+
+
+class HomePosts(ListView):
+    model = Posts
+    template_name = 'Short_tales/index.html'
+    context_object_name = 'posts'
+    # extra_context = {'title': 'Главная'}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        return context
+
+    def get_queryset(self):
+        return Posts.objects.filter(posted=True)
+
 
 def index(request):
     posts = Posts.objects.all()
