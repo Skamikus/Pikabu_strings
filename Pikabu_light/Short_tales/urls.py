a7f59c4eea22cf3ae9703b9f9c5f5ee9a3a19c57
Maxim Budilov <lordgodrik@gmail.com>
2022-07-08 23:12:44 +0300

    simple registration

--- Pikabu_light/Short_tales/urls.py
+++ Pikabu_light/Short_tales/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
     path('', HomePosts.as_view(), name='home'),
     path('category/<int:category_id>/', CategoryPosts.as_view(), name='category'),
     path('posts/add-post/', AddPosts.as_view(), name='add_post'),
--- Pikabu_light/Short_tales/views.py
+++ Pikabu_light/Short_tales/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.views.generic import ListView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 from .models import Posts, Category
 from .forms import PostsForm
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Регистрация прошла успешно')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserCreationForm()
+    return render(request, 'Short_tales/register.html', {'form': form})
+
+def login(request):
+    return render(request, 'Short_tales/login.html')
 
 class HomePosts(ListView):
     model = Posts
